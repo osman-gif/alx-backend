85c5d797e21e84c26b955f41366882429d487e5e
osman-gif <osmanngong@gmail.com>
2024-11-06 11:22:05 +0200

    Added the 0x02-i18n project

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+First you will setup a basic Flask app in 0-app.py. Create a single / route
+and an index.html template that simply outputs “Welcome to Holberton” as
+page title (<title>) and “Hello world” as header (<h1>).
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+app.route('/')
+def index():
+    """Return index.html"""
+    return render_template('index.html')
