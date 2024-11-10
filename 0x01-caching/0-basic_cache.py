3ba9528c6d59111317069831977bb8e7321b296d
osman-gif <osmanngong@gmail.com>
2024-11-10 09:45:32 +0200

    Documentd the put and get functions

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -26,10 +26,16 @@ class BasicCache(BaseCaching):
     """
 
     def put(self, key, item):
+        """
+        Add an item in the cache
+        """
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
+        """
+        Get an item by key
+        """
         if key is not None:
             if key not in self.cache_data.keys():
                 return None
