9833539054d0dc4823826d3994fbae5325e3eeb4
osman-gif <osmanngong@gmail.com>
2024-11-17 10:34:25 +0200

    documented the 2-lifo_cache.py file functions

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -9,7 +9,9 @@ class LIFOCache(BaseCaching):
         super().__init__()
 
     def put(self, key, item):
-
+        """
+        Add an item in the cache
+        """
         if key is not None and item is not None:
             keys = list(self.cache_data.keys())
             if len(self.cache_data.items()) >= self.MAX_ITEMS:
@@ -22,6 +24,10 @@ class LIFOCache(BaseCaching):
             self.cache_data[key] = item
 
     def get(self, key):
+        """
+        Get an item by key
+        """
+
         if key is not None:
             if key not in self.cache_data.keys():
                 return None
